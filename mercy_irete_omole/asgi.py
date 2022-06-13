b6d5968a3b5a2a7ff1b41b0dca31f26a4d68c8d0
Mercy <mercyomole229@gmail.com>
2022-06-13 18:35:03 +0100

    initial commit

---
+++ mercy_irete_omole/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mercy_irete_omole project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mercy_irete_omole.settings')
+
+application = get_asgi_application()
